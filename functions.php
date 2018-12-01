be5201b679ee3ed7b41cd750fa2d9328af7d61eb
geeac <eugenia.cosinschi@gmail.com>
2018-12-01 10:42:37 +0200

    Initial commit

---
+++ functions.php
@@ -0,0 +1,394 @@
+<?php
+/**
+ * Genesis Sample.
+ *
+ * This file adds functions to the Genesis Sample Theme.
+ *
+ * @package Genesis Sample
+ * @author  StudioPress
+ * @license GPL-2.0+
+ * @link    https://www.studiopress.com/
+ */
+
+// Starts the engine.
+require_once get_template_directory() . '/lib/init.php';
+
+// Sets up the Theme.
+require_once get_stylesheet_directory() . '/lib/theme-defaults.php';
+
+add_action( 'after_setup_theme', 'genesis_sample_localization_setup' );
+/**
+ * Sets localization (do not remove).
+ *
+ * @since 1.0.0
+ */
+function genesis_sample_localization_setup() {
+
+	load_child_theme_textdomain( 'dreamplanner', get_stylesheet_directory() . '/languages' );
+
+}
+
+// Adds helper functions.
+require_once get_stylesheet_directory() . '/lib/helper-functions.php';
+
+// Adds image upload and color select to Customizer.
+require_once get_stylesheet_directory() . '/lib/customize.php';
+
+// Includes Customizer CSS.
+require_once get_stylesheet_directory() . '/lib/output.php';
+
+// Adds WooCommerce support.
+require_once get_stylesheet_directory() . '/lib/woocommerce/woocommerce-setup.php';
+
+// Adds the required WooCommerce styles and Customizer CSS.
+require_once get_stylesheet_directory() . '/lib/woocommerce/woocommerce-output.php';
+
+// Adds the Genesis Connect WooCommerce notice.
+require_once get_stylesheet_directory() . '/lib/woocommerce/woocommerce-notice.php';
+
+// Defines the child theme (do not remove).
+define( 'CHILD_THEME_NAME', 'Dream Planner' );
+define( 'CHILD_THEME_URL', 'https://greatoakcircle.com/' );
+define( 'CHILD_THEME_VERSION', '2.6.0' );
+
+add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
+/**
+ * Enqueues scripts and styles.
+ *
+ * @since 1.0.0
+ */
+function genesis_sample_enqueue_scripts_styles() {
+
+	wp_enqueue_style( 'dashicons' );
+
+	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+	wp_enqueue_script(
+		'genesis-sample-responsive-menu',
+		get_stylesheet_directory_uri() . "/js/responsive-menus{$suffix}.js",
+		array( 'jquery' ),
+		CHILD_THEME_VERSION,
+		true
+	);
+	wp_localize_script(
+		'genesis-sample-responsive-menu',
+		'genesis_responsive_menu',
+		genesis_sample_responsive_menu_settings()
+	);
+
+	wp_enqueue_script(
+		'genesis-sample',
+		get_stylesheet_directory_uri() . '/js/genesis-sample.js',
+		array( 'jquery' ),
+		CHILD_THEME_VERSION,
+		true
+	);
+
+}
+
+/**
+ * Defines responsive menu settings.
+ *
+ * @since 2.3.0
+ */
+function genesis_sample_responsive_menu_settings() {
+
+	$settings = array(
+		'mainMenu'         => __( 'Menu', 'dreamplanner' ),
+		'menuIconClass'    => 'dashicons-before dashicons-menu',
+		'subMenu'          => __( 'Submenu', 'dreamplanner' ),
+		'subMenuIconClass' => 'dashicons-before dashicons-arrow-down-alt2',
+		'menuClasses'      => array(
+			'combine' => array(
+				'.nav-primary',
+			),
+			'others'  => array(),
+		),
+	);
+
+	return $settings;
+
+}
+
+// Sets the content width based on the theme's design and stylesheet.
+if ( ! isset( $content_width ) ) {
+	$content_width = 702; // Pixels.
+}
+
+// Adds support for HTML5 markup structure.
+add_theme_support(
+	'html5', array(
+		'caption',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'search-form',
+	)
+);
+
+// Adds support for accessibility.
+add_theme_support(
+	'genesis-accessibility', array(
+		'404-page',
+		'drop-down-menu',
+		'headings',
+		'rems',
+		'search-form',
+		'skip-links',
+	)
+);
+
+// Adds viewport meta tag for mobile browsers.
+add_theme_support(
+	'genesis-responsive-viewport'
+);
+
+// Adds custom logo in Customizer > Site Identity.
+add_theme_support(
+	'custom-logo', array(
+		'height'      => 120,
+		'width'       => 700,
+		'flex-height' => true,
+		'flex-width'  => true,
+	)
+);
+
+// Renames primary and secondary navigation menus.
+add_theme_support(
+	'genesis-menus', array(
+		'primary'   => __( 'Header Left', 'dreamplanner' ),
+		'secondary' => __( 'Header Right', 'dreamplanner' ),
+	)
+);
+
+// Adds support for after entry widget.
+add_theme_support( 'genesis-after-entry-widget-area' );
+
+// Adds support for 3-column footer widgets.
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+// Removes header right widget area.
+unregister_sidebar( 'header-right' );
+
+// Removes secondary sidebar.
+unregister_sidebar( 'sidebar-alt' );
+
+// Removes site layouts.
+genesis_unregister_layout( 'content-sidebar-sidebar' );
+genesis_unregister_layout( 'sidebar-content-sidebar' );
+genesis_unregister_layout( 'sidebar-sidebar-content' );
+
+// Removes output of primary navigation right extras.
+remove_filter( 'genesis_nav_items', 'genesis_nav_right', 10, 2 );
+remove_filter( 'wp_nav_menu_items', 'genesis_nav_right', 10, 2 );
+
+add_action( 'genesis_theme_settings_metaboxes', 'genesis_sample_remove_metaboxes' );
+/**
+ * Removes output of unused admin settings metaboxes.
+ *
+ * @since 2.6.0
+ *
+ * @param string $_genesis_admin_settings The admin screen to remove meta boxes from.
+ */
+function genesis_sample_remove_metaboxes( $_genesis_admin_settings ) {
+
+	remove_meta_box( 'genesis-theme-settings-header', $_genesis_admin_settings, 'main' );
+	remove_meta_box( 'genesis-theme-settings-nav', $_genesis_admin_settings, 'main' );
+
+}
+
+add_filter( 'genesis_customizer_theme_settings_config', 'genesis_sample_remove_customizer_settings' );
+/**
+ * Removes output of header settings in the Customizer.
+ *
+ * @since 2.6.0
+ *
+ * @param array $config Original Customizer items.
+ * @return array Filtered Customizer items.
+ */
+function genesis_sample_remove_customizer_settings( $config ) {
+
+	unset( $config['genesis']['sections']['genesis_header'] );
+	return $config;
+
+}
+
+// Displays custom logo.
+add_action( 'genesis_site_title', 'the_custom_logo', 0 );
+
+// Repositions primary navigation menu.
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_header', 'genesis_do_nav', 8 );
+
+// Repositions the secondary navigation menu.
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+add_action( 'genesis_header', 'genesis_do_subnav', 9 );
+
+add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
+/**
+ * Reduces secondary navigation menu to one level depth.
+ *
+ * @since 2.2.3
+ *
+ * @param array $args Original menu options.
+ * @return array Menu options with depth set to 1.
+ */
+function genesis_sample_secondary_menu_args( $args ) {
+
+	if ( 'secondary' !== $args['theme_location'] ) {
+		return $args;
+	}
+
+	$args['depth'] = 1;
+	return $args;
+
+}
+
+add_filter( 'genesis_author_box_gravatar_size', 'genesis_sample_author_box_gravatar' );
+/**
+ * Modifies size of the Gravatar in the author box.
+ *
+ * @since 2.2.3
+ *
+ * @param int $size Original icon size.
+ * @return int Modified icon size.
+ */
+function genesis_sample_author_box_gravatar( $size ) {
+
+	return 90;
+
+}
+
+add_filter( 'genesis_comment_list_args', 'genesis_sample_comments_gravatar' );
+/**
+ * Modifies size of the Gravatar in the entry comments.
+ *
+ * @since 2.2.3
+ *
+ * @param array $args Gravatar settings.
+ * @return array Gravatar settings with modified size.
+ */
+function genesis_sample_comments_gravatar( $args ) {
+
+	$args['avatar_size'] = 60;
+	return $args;
+
+}
+// Remove Entry Header from homepage
+add_action( 'genesis_before', 'gc_remove_entry_header' );
+function gc_remove_entry_header() {
+
+	if ( is_home() && ! is_page() ) { return; }
+
+	//* Remove the entry header markup (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+	remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+
+	//* Remove the entry title (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+
+	//* Remove the entry meta in the entry header (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+
+	//* Remove the post format image (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_do_post_format_image', 4 );
+}
+
+//* Add sidebar to blog posts
+add_action( 'genesis_pre_get_option_site_layout', 'gc_add_primary_sidebar' );
+function gc_add_primary_sidebar() {
+	if ( is_home() || is_singular('post') || is_category() || is_month() ) { // for archives and single post pages
+		$opt = 'content-sidebar'; 
+    		return $opt;
+	}
+}
+
+// Full Width Pages on WooCommerce
+function gc_fullwidth_layout() {
+	if( is_page ( array( 'cart', 'checkout' )) || is_shop() || 'product' == get_post_type() ) {
+		return 'full-width-content';
+	}
+}
+add_filter( 'genesis_site_layout', 'gc_fullwidth_layout' );
+
+// Reposition featured image before entry title
+remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
+add_action( 'genesis_entry_header', 'genesis_do_post_image', 5 );
+
+// Change the footer credits
+add_filter('genesis_footer_creds_text', 'sp_footer_creds_filter');
+function sp_footer_creds_filter( $creds ) {
+	$creds = 'Copyright [footer_copyright] &middot; <a href="'.get_bloginfo( 'url' ).'">'. get_bloginfo( 'name' ) .'</a>. Design by <a href="http://greatoakcircle.com" target="_blank">Great Oak Circle</a>.';
+	return $creds;
+}
+
+// Add widget to primary navigation
+add_filter( 'genesis_nav_items', 'gc_social_icons', 10, 2 );
+add_filter( 'wp_nav_menu_items', 'gc_social_icons', 10, 2 );
+
+function gc_social_icons($menu, $args) {
+	$args = (array)$args;
+	if ( 'primary' !== $args['theme_location'] )
+		return $menu;
+	ob_start();
+	genesis_widget_area('nav-social-menu');
+	$social = ob_get_clean();
+	return $menu . $social;
+}
+
+// The Event calendar fix
+add_action( 'get_header', 'tribe_genesis_bypass_genesis_do_post_content' );
+function tribe_genesis_bypass_genesis_do_post_content() {
+ 
+    if ( class_exists( 'Tribe__Events__Main' ) && class_exists( 'Tribe__Events__Pro__Main' ) ) {
+        if ( tribe_is_month() || tribe_is_upcoming() || tribe_is_past() || tribe_is_day() || tribe_is_map() || tribe_is_photo() || tribe_is_week() || ( tribe_is_recurring_event() && ! is_singular( 'tribe_events' ) ) ) {
+            remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
+            remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
+            add_action( 'genesis_entry_content', 'the_content', 15 );
+        }
+    } elseif ( class_exists( 'Tribe__Events__Main' ) && ! class_exists( 'Tribe__Events__Pro__Main' ) ) {
+        if ( tribe_is_month() || tribe_is_upcoming() || tribe_is_past() || tribe_is_day() ) {
+            remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
+            remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
+            add_action( 'genesis_entry_content', 'the_content', 15 );
+        }
+    }
+ 
+}
+// WC products per page
+add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 20 );
+
+function new_loop_shop_per_page( $cols ) {
+  // $cols contains the current number of products per page based on the value stored on Options -> Reading
+  // Return the number of products you wanna show per page.
+  $cols = 100;
+  return $cols;
+}
+
+genesis_register_sidebar( array(
+	'id'          	=> 'nav-social-menu',
+	'name'        	=> __( 'Nav Social Menu', 'text-domain' ),
+	'description' 	=> __( 'This is the social media section that appears in the menu above the header.', 'text-domain' ),
+) );
+
+// Add Top Site widget area
+add_action( 'genesis_before_header', 'gc_display_top_site', 10 );
+function gc_display_top_site() {
+	genesis_widget_area('top-site',
+		array(
+           	 'before' => '<div class="top-site"><div class="wrap">',
+		'after'   => '</div></div>',
+        	)
+	);
+}
+genesis_register_sidebar( array(
+	'id'          	=> 'top-site',
+	'name'        	=> __( 'Top Site', 'dreamplanner' ),
+	'description' 	=> __( 'This is the top site widget area.', 'dreamplanner' ),
+) );
+
+// Register the optin sidebar
+genesis_register_sidebar( array(
+	'id'          => 'optin',
+	'name'        => __( 'Optin', 'dreamplanner' ),
+	'description' => __( 'This is the optin widget area.', 'dreamplanner' ),
+) );
